2b19fba3c7ccedefccee3191ba5664da7e7a8cdb
Zhiguang Wang <wangzhiguang@kingsoft.com>
2017-12-20 14:06:07 +0800

    Refactor linter.sh

--- linter.sh
+++ linter.sh
@@ -2,7 +2,10 @@
 
 JENKINS_URL=https://jenkins.intranet.rog2.org
 
-JENKINS_FILE=$1
+jenkinsfile=$1
+crumb_header=$(curl -s "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
 
-JENKINS_CRUMB=$(curl -s "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
-curl -X POST -H $JENKINS_CRUMB -F "jenkinsfile=<$JENKINS_FILE" $JENKINS_URL/pipeline-model-converter/validate
+curl -s -X POST \
+    -H "$crumb_header" \
+    -F "jenkinsfile=<$jenkinsfile" \
+    "$JENKINS_URL/pipeline-model-converter/validate"
